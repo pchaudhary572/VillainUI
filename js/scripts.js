0e1bd3560cb8a6c92c84e22e87fdd79cbc6973f1
Achyut Timsina <actimsina@gmail.com>
2019-05-09 16:58:20 +0545

    image uploads for each hero

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,16 @@
 $(function () {
-    // $.getJSON("http://localhost:3000/heroes", function (heroes) {
-    //     console.log(heroes);
-    //     let myRows = [];
-    //     $.each(heroes, function (index, hero) {
-    //         myRows.push("<tr><td>" + hero.name + "</td><td>" + hero.desc + "</td></tr>");
-    //     });
-    //     $("#tblbody").append(myRows);
-    // });
     let tblBody = $("#tblbody");
     $.ajax({
         type: 'GET',
         url: 'http://localhost:3000/heroes',
         success: function (heroes) {
             let myRows = [];
+
             $.each(heroes, function (index, hero) {
-                myRows.push("<tr><td>" + hero.name + "</td><td>" + hero.desc + "</td></tr>");
+                source = 'http://localhost:3000/uploads/' + hero.image;
+                thisRow = "<tr><td>" + hero.name + "</td><td>" + hero.desc + "</td>";
+                thisRow += "<td><img src= " + source + " width='60' /></td></tr>";
+                myRows.push(thisRow);
             });
             tblBody.append(myRows);
         },
@@ -23,23 +19,50 @@ $(function () {
         }
     });
 
+
     $("#add-hero").on('click', function () {
-        let hero = {
-            name: $("#name").val(),
-            desc: $("#desc").val()
-        };
+        let formData = new FormData();
+        let files = $("#fileToUpload").get(0).files;
+        if (files.length > 0) {
+            formData.append("imageFile", files[0]);
+        }
 
         $.ajax({
             type: 'POST',
-            url: 'http://localhost:3000/heroes',
-            data: hero,
-            success: function (newHero) {
-                tblBody.append("<tr><td>" + newHero.name + "</td><td>" + newHero.desc + "</td></tr>")
+            url: 'http://localhost:3000/upload',
+            contentType: false,
+            cache: false,
+            processData: false,
+            data: formData,
+            success: function (data) {
+                let hero = {
+                    name: $("#name").val(),
+                    desc: $("#desc").val(),
+                    image: data.filename
+                };
+                $.ajax({
+                    type: 'POST',
+                    url: 'http://localhost:3000/heroes',
+                    data: hero,
+                    success: function (hero) {
+                        source = 'http://localhost:3000/uploads/' + hero.image;
+                        thisRow = "<tr><td>" + hero.name + "</td><td>" + hero.desc + "</td>";
+                        thisRow += "<td><img src= " + source + " width='60' /></td></tr>";
+
+                        tblBody.append(thisRow);
+                        $('#hero-form').trigger('reset');
+                    },
+                    error: function (error) {
+                        alert(error);
+                    }
+                });
             },
-            error: function () {
-                alert("Couldn't save hero");
+            error: function (error) {
+                console.log(error);
             }
-        });
+        })
+
+
     });
 
     $("#remove-heroes").on('click', function () {
@@ -54,5 +77,5 @@ $(function () {
                 alert("Couldn't delete all heroes");
             }
         });
-    })
+    });
 });
